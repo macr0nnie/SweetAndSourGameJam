3616c8d6b3db1690726bb3e5dd842b274bface48
macr0nnie <macr0nnieasmr@gmail.com>
2023-03-03 20:02:44 -0500

    d
    d

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private float Speed = 5f;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //GET MOVEMENT INPUT
     }
+    void FixedUpdate(){
+        //GET MOVEMENT
+
+    }
+    void GetInput(){
+    
+    }
+
 }
